5cb85e3b2a1ebb45da546b76225605d71b70b1d5
Jetheesan <jetheesan@gmail.com>
2024-06-13 16:21:07 +0100

    testing the top_movies method

---
+++ api/tests/test_movie_repository/test_get_top_movies.py
@@ -0,0 +1,77 @@
+from lib.movie_repository import MovieRepository
+import json
+import os
+import datetime
+
+"""
+test that shows the top 2 movies from the test database
+"""
+
+def test_find_top_movies_returns_top_movies():
+    movie_repo =MovieRepository()
+    result = movie_repo.find_top_movies("test_movie_database")
+    expected_result = [
+  {
+    "_id": "666af5493ea95c2d6684bd78",
+    "adult": False,
+    "backdrop_path": "/random",
+    "genre_ids": [
+      18,
+      80
+    ],
+    "id": 278,
+    "original_language": "en",
+    "original_title": "Title1",
+    "overview": "Overview1",
+    "popularity": 143.5,
+    "poster_path": "/path1",
+    "release_date": datetime.datetime(1994, 9, 23, 0, 0),
+    "title": "Title1",
+    "video": False,
+    "vote_average": 8.705,
+    "vote_count": 50
+  },
+  {
+    "_id": "666af5493ea95c2d6684bd79",
+    "adult": False,
+    "backdrop_path": "/random1",
+    "genre_ids": [
+      9,
+      75
+    ],
+    "id": 238,
+    "original_language": "en",
+    "original_title": "Title2",
+    "overview": "Overview2",
+    "popularity": 142.2,
+    "poster_path": "/path2",
+    "release_date": datetime.datetime(1972, 3, 14, 0, 0),
+    "title": "Title2",
+    "video": False,
+    "vote_average": 8.694,
+    "vote_count": 100
+  },
+  {
+    "_id": "666af5493ea95c2d6684bd7a",
+    "adult": False,
+    "backdrop_path": "/random2",
+    "genre_ids": [
+      18,
+      80
+    ],
+    "id": 299,
+    "original_language": "en",
+    "original_title": "Title3",
+    "overview": "Overview3",
+    "popularity": 142.2,
+    "poster_path": "/path3",
+    "release_date": datetime.datetime(1972, 3, 14, 0, 0),
+    "title": "Title3",
+    "video": False,
+    "vote_average": 7.0,
+    "vote_count": 150
+  }
+]
+
+    
+    assert result == expected_result

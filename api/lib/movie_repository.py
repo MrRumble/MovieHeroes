2d4f6c5e51fc103b7b81349c3021481a90f9a01e
James Rumble <jamesrumble@live.com>
2024-06-13 11:08:44 +0100

    find top 12 movies method from db working

--- api/lib/movie_repository.py
+++ api/lib/movie_repository.py
@@ -1,5 +1,6 @@
-import movie
-import database_connection  
+from lib.movie import Movie
+from lib import database_connection
+from pymongo import DESCENDING
 
 class MovieRepository:
     def __init__(self):
@@ -8,7 +9,7 @@ class MovieRepository:
     def find_movie_by_id(self, id):
         movies = self.db["Movie_Heros"]
         found_movie = movies.find_one({"id": id})
-        return movie.Movie(
+        return Movie(
             found_movie["id"],
             found_movie["title"],
             found_movie["overview"],
@@ -17,6 +18,29 @@ class MovieRepository:
             found_movie["vote_average"],
             found_movie["release_date"]
             )
+    
+    def find_top_twelve_movies(self):
+        movies = self.db["Movie_Heros"]
+        sorted_db_vote_average = movies.find().sort("vote_average", DESCENDING).limit(12)
+        movies_as_dicts = list(sorted_db_vote_average)
+        print(type(sorted_db_vote_average))
+        print(type(movies_as_dicts[0]))
+        movies_objects = []
+        for movie in movies_as_dicts:
+            movie_to_add = Movie(
+                movie["id"],
+                movie["title"],
+                movie["overview"],
+                movie["poster_path"],
+                movie["backdrop_path"],
+                movie["vote_average"],
+                movie["release_date"]
+            )
+            movies_objects.append(movie_to_add)
+        return movies_objects
 
+
+    
 movies = MovieRepository()
 print(movies.find_movie_by_id(238))
+print(movies.find_top_twelve_movies())
--- api/routes/landing_page.py
+++ api/routes/landing_page.py
@@ -1,9 +1,12 @@
 from app import app
 from flask import Flask, request, jsonify, redirect
 from dotenv import load_dotenv
-import requests
+from lib.movie_repository import MovieRepository
+
 
 @app.route("/", methods=["GET"])
-def getpost():   # Will be refactored later to point to top 12 films from db.
-    return "Post" 
+def get_top_twelve_movies(): 
+    movie_repo = MovieRepository()
+    top_movies = movie_repo.find_top_twelve_movies()
+    return top_movies 
 

af066409537b6d900b680ab98361d41129b75cb9
Anko21 <am.kontaratou@gmail.com>
2024-06-14 17:19:40 +0100

    added userId starting from 611 onwards

--- api/lib/user.py
+++ api/lib/user.py
@@ -1,12 +1,13 @@
 class User:
-    def __init__(self, full_name, email, password, _id=None):
+    def __init__(self, full_name, email, password, _id=None, userId = None):
         self.id = _id
         self.full_name = full_name
         self.email = email
         self.password = password
+        self.userId = userId
     
     def __repr__(self):
-        return f"User({self.id}, {self.full_name}, {self.email}, {self.password})"
+        return f"User({self.id}, {self.full_name}, {self.email}, {self.password}, {self.userId})"
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__

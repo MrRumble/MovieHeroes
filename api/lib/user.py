959362d999119df745245bf93119812cbeca5eaa
Anko21 <am.kontaratou@gmail.com>
2024-06-18 11:49:44 +0100

    adding user model, error

---
+++ api/lib/user.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self, full_name, email, password, _id=None, userId =None, userRating = [], avatar = ""):
+        self.id = _id
+        self.full_name = full_name
+        self.email = email
+        self.password = password
+        self.userId = userId
+        self.userRating = userRating
+        self.userAvatar = avatar
+
+
+    
+    def __repr__(self):
+        return f"User({self.id}, {self.full_name}, {self.email}, {self.password}, {self.userId}, {self.avatar}, {self.userRating})"
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__

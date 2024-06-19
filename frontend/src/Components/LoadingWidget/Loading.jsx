f899bde51b7066faf81eabc4663f5911de032039
James Rumble <jamesrumble@live.com>
2024-06-19 13:12:03 +0100

    Loading Widget

---
+++ frontend/src/Components/LoadingWidget/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loading.css'; // Make sure to import the CSS file
+
+const Loading = () => {
+    return (
+        <div className="spinner-container">
+            <div className="spinner"></div>
+        </div>
+    );
+};
+
+export default Loading;

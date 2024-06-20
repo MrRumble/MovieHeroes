071f8f63f42f214e92a4d6ecf30177d1cdc34930
Anko21 <am.kontaratou@gmail.com>
2024-06-20 12:08:49 +0100

    css profile page done/ corrected sign up btns

--- frontend/src/pages/MyProfilePage/MyProfile.jsx
+++ frontend/src/pages/MyProfilePage/MyProfile.jsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from "react";
 import Navbar from "../../Components/Navbar";
 import "./MyProfile.css"; 
 import {updateAvatar, getAvatarByUserById} from "../../services/user"
+import BackdropSlider from "../../Components/BackdropSlider/BackdropSlider"; // Import the BackdropSlider component
+import { useNavigate } from "react-router-dom";
 
 const MyProfilePage = () => {
   const [selectedAvatar, setSelectedAvatar] = useState(null);
   const [userAvatar, setUserAvatar] = useState("")
   const [isVisible, setIsVisible] = useState(false)
+  const navigate = useNavigate();
 
   const fullName = localStorage.getItem("full_name");
   const email = localStorage.getItem("email");
@@ -52,51 +55,52 @@ const MyProfilePage = () => {
   }, [token]);
 
   return (
-    <div className="my-profile-page">
-      <Navbar userName={fullName} /> 
-      <h1 className="profile-heading">MY PROFILE</h1>
-      <div className="profile-details">
-        <p>
-          {userAvatar? <img src={userAvatar} className="user-avatar"alt='avatar'/> : ""}
-        </p>
-        <button onClick={()=>setIsVisible(!isVisible)}>
-          {isVisible? "Cancel" : "Choose your Hero"}
-        </button>
+    <div >
+      <Navbar userName={fullName} />
+      <BackdropSlider /> {/* Render the BackdropSlider component */}
+      <div className="my-profile-page">
+        <h2 className="profile-heading">{fullName}'s Profile</h2>
+        <div className="profile-detailsPage">
+          <p>
+            {userAvatar? <img src={userAvatar} className="user-avatar"alt='avatar'/> : ""}
+          </p>
+          <button onClick={()=>setIsVisible(!isVisible)} id="selectAvatar" >
+            {isVisible? "Cancel" : "Choose your Hero"}
+          </button>
 
-        {isVisible && (
-          <div className="avatar-selection">
-          <h3>Choose Your Avatar:</h3>
-          <form onSubmit={handleSubmit}>
-            <div className="avatar-options">
-              {avatarOptions.map((avatar, index) => (
-                <img
-                  key={index}
-                  src={avatar}
-                  alt={`Avatar ${index}`}
-                  className={`avatar ${avatar === selectedAvatar ? 'selected' : ''}`}
-                  value={avatar}
-                  onClick={() => setSelectedAvatar(avatar)}
-                />
-              ))}
-            </div>
-            <br/>
-            <div>
-              <input role="submit-button" id="submit" type="submit" value="Save Avatar" />
-            </div>
-          </form>
+          {isVisible && (
+            <div className="avatar-selection">
+              <h3>Choose Your Avatar:</h3>
+              <form onSubmit={handleSubmit}>
+                <div className="avatar-options">
+                  {avatarOptions.map((avatar, index) => (
+                    <img
+                      key={index}
+                      src={avatar}
+                      alt={`Avatar ${index}`}
+                      className={`avatar ${avatar === selectedAvatar ? 'selected' : ''}`}
+                      value={avatar}
+                      onClick={() => setSelectedAvatar(avatar)}
+                    />
+                  ))}
+                </div>
+                <br/>
+                <div>
+                  <input role="submit-button" id="submit"  type="submit" value="Save Avatar" />
+                </div>
+              </form>
+          </div>
+            )}
+          <div className="user-details">
+            <p>
+              <strong>User Name:</strong> {fullName || "Not available"}
+            </p>
+            <p>
+              <strong>Email:</strong> {email || "Not available"}
+            </p>
+          </div>
         </div>
-        )}
-
-        <p>
-          <strong>User Name:</strong> {fullName || "Not available"}
-        </p>
-        <p>
-          <strong>Email:</strong> {email || "Not available"}
-        </p>
-        <p>
-          <strong>My Favourite Films:</strong>
-        </p>
-        Top ranked films...?
+        <button onClick={()=>{navigate("/initial_ratings")}} id="selectAvatar">Rate your favorite movies</button>
       </div>
     </div>
   );

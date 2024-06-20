fd3ca8a2ad528739cf7878fa4a9cec914e5fe1e0
James Rumble <jamesrumble@live.com>
2024-06-20 07:45:23 +0100

    Bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,6 +51,7 @@ const router = createBrowserRouter([
     path: "/initial_ratings",
     element: <InititalRatingsPage/>
   },
+  {
     path: "/movie_results",
     element: <MovieSearchResultsPage/>
   }
